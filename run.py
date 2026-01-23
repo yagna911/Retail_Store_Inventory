e5e03bfc7c6b984d5cebf2846c2fd826b13909b2
Yagna yamala <146903789+yagna911@users.noreply.github.com>
2026-01-23 23:02:51 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,53 @@
+from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog, QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
+QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit, QMessageBox, QWidget, QMainWindow,
+QVBoxLayout)
+import sys
+
+from Application.Components.Dashboard import DashboardView
+
+class Boot(QDialog):
+    def __init__(self, parent=None):
+        super(Boot, self).__init__(parent)
+        self.createFormGroupBox()
+        self.buttonBox = QDialogButtonBox()
+        self.buttonBox.addButton("Login", QDialogButtonBox.AcceptRole)
+        self.buttonBox.addButton("Cancel", QDialogButtonBox.RejectRole)
+        self.buttonBox.accepted.connect(self.onClickLogin)
+        self.buttonBox.rejected.connect(self.reject)
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.formGroupBox)
+        self.mainLayout.addWidget(self.buttonBox)
+        self.setLayout(self.mainLayout)
+        self.setWindowTitle("Login")
+        
+    def createFormGroupBox(self):
+        self.formGroupBox = QGroupBox("Login")
+        self.layout = QFormLayout()
+        self.password = QLineEdit()
+        self.password.setEchoMode(QLineEdit.Password)
+        self.username = QLineEdit()
+        self.layout.addRow(QLabel("Username:"), self.username)
+        self.password.setObjectName("password")
+        self.layout.addRow(QLabel("Password:"), self.password)
+        self.formGroupBox.setLayout(self.layout)
+
+    def onClickLogin(self):
+        if self.username.text() == "yagna" and self.password.text() == "2005":
+        #if self.username.text() == "":
+           self.accept()
+        else:
+         self.msg = QMessageBox()
+         self.msg.setText("Error")
+         self.msg.setInformativeText('Incorrect username or password')
+         self.msg.setWindowTitle("Sign In Error")
+         self.msg.exec_()
+
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle("fusion")
+    bootUI = Boot()
+    if bootUI.exec_() == QDialog.Accepted:
+        main_window = DashboardView()
+        main_window.show()
+        sys.exit(app.exec_())
